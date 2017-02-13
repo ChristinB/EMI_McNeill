ae4e0d1e715c1115ed01301be0a70c6258857fa2
ChristinB <Christin.Bobe@UGent.be>
2017-02-13 14:55:36 +0100

    Add files via upload

---
+++ EMIforward_DualEM.m
@@ -0,0 +1,71 @@
+clear all
+close all
+
+%% Device parameters (VCP)
+%Coil spacing [m]
+s1=1;
+s2=2;
+
+%% Layer properties
+%Top layer
+cond1=5e-5; %[S/m]
+
+%Thin layer TL
+DepthTL=0.5; %[m]
+cond2=20e-3; %[S/m]
+
+% Towards phi
+Depth=0:0.05:2; %[m]
+z1=Depth/s1; %normalized Depth by Coil spacing
+z2=Depth/s2;
+
+%for coil spacing 1 PRP
+%phi is the sensitivity
+%R is the relative contribution to Hs from all material below z1
+Phiz1P=2./(4*z1.^2+1).^(3/2); 
+Rz1P=2*z1./(4*z1.^2+1).^(1/2);
+
+%for coil spacing 1 HCP
+Phiz1H=4*z1./(4*z1.^2+1).^(3/2); 
+Rz1H=1-1./(sqrt(4*z1.^2+1));
+
+%Representation of relative influence of current flow as a function of
+%depth
+figure
+hold on
+subplot(2,1,1)
+hold on
+plot(z1,Phiz1P,'-')
+plot(z1,Phiz1H,'-')
+xlabel('Normalized Depth')
+ylabel('Phi(z1)')
+title('Sensitivity as a function of depth')
+h = legend ({"PRP"}, "HCP");
+legend (h, "location", "northeast");
+set (h, "fontsize", 20);
+
+
+subplot(2,1,2)
+hold on
+plot(z1,Rz1P,'-')
+plot(z1,Rz1H,'-')
+xlabel('Normalized Depth')
+ylabel('R(z1)')
+title('Cumulutive response versus depth')
+
+
+
+%% From conductivities to apparent conductivities (measured by device)
+%conda1=cond1*(1-Rz1)+cond2*Rz1;
+%conda2=cond1*(1-Rz2)+cond2*Rz2;
+%
+%figure
+%hold on
+%plot(conda1,Rz1,'*g')
+%plot(conda2,Rz2,'*r')
+%title('Apparent conductivity according to depth z1 of layer boundary')
+%xlabel('Normalized Depth')
+%ylabel('R(z2)')
+
+
+
